e56588b306c04317a42f8874efb0c0cd05e55f44
pyxsor <hanzdeveloper@gmail.com>
2024-06-07 23:45:36 +0700

    feat(component): add layout footer

---
+++ src/component/layout/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer
+            className="absolute bottom-0 w-full left-0 py-1 flex text-center items-center justify-center font-semibold bg-primary text-light-gray ">
+                <span>
+                    <a className=" flex max-sm:flex-col font-bold" href="https://linkedin.com/id/handie">
+                        Handie Pramana Putra
+                    </a>
+                </span>
+        </footer>
+    );
+}
+
+export default Footer;
